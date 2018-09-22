132e70af0245610dad5e61f45c1562d4229e2d38
catamphetamine <purecatamphetamine@gmail.com>
2018-09-22 22:43:27 +0300

    Styling <Post/>

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import ReactTimeAgo from 'react-time-ago'
+import { Link } from 'react-website'
+
 import { postShape } from '../PropTypes'
+import { accountLink } from './AccountLink'
+import AccountPicture from './AccountPicture'
+
+import './Post.css'
 
 export default function Post({ post }) {
 	return (
 		<div className="post">
-			{post.account.name}
+			<div className="post__summary">
+				<Link to={accountLink(post.account)}>
+					<AccountPicture
+						account={post.account}
+						className="post__account-picture"/>
+				</Link>
+				<div className="post__name-and-date">
+					<Link to={accountLink(post.account)}>
+						{post.account.name}
+					</Link>
+					<div className="post__date">
+						<ReactTimeAgo tooltipClassName="post__date-tooltip">
+							{post.date}
+						</ReactTimeAgo>
+					</div>
+				</div>
+			</div>
 			{post.content && post.content.map((content, i) => {
 				if (typeof content === 'string') {
 					return (
