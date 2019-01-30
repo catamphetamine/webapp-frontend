4c8b7da1a838f1b225b2daefaa4cfbdc123caddc
catamphetamine <purecatamphetamine@gmail.com>
2019-01-30 23:49:47 +0300

    Anchor links

--- src/components/PostLink.js
+++ src/components/PostLink.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-website'
 import classNames from 'classnames'
 
 import './PostLink.css'
 
 export default function PostLink({ url, className, children }) {
+	if (url[0] === '/') {
+		return (
+			<Link
+				to={url}
+				className={classNames('post__link', className)}>
+				{children}
+			</Link>
+		)
+	}
 	return (
 		<a
-			target="_blank"
+			target={url[0] === '#' ? undefined : '_blank'}
 			href={url}
 			className={classNames('post__link', className)}>
 			{children}
