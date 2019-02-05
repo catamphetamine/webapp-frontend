86adc2afdba9dd1dba8c162a91ecc43f768fd81a
catamphetamine <purecatamphetamine@gmail.com>
2019-02-05 08:46:13 +0300

    <PostVideo/> title and link

--- src/components/PostVideo.js
+++ src/components/PostVideo.js
@@ -4,6 +4,8 @@ import { videoAttachmentShape } from '../PropTypes'
 import Picture from './Picture'
 import Video from './Video'
 
+import { getVideoURL } from '../utility/video'
+
 import './PostVideo.css'
 
 export default class PostVideo extends React.Component {
@@ -15,14 +17,33 @@ export default class PostVideo extends React.Component {
 				video
 			}
 		} = this.props
+
+		let url
+		if (video.source.provider) {
+			url = getVideoURL(video.source.id, video.source.provider)
+		}
+
 		return (
-			<Video
-				fit={height ? 'height' : undefined}
-				maxHeight={height}
-				video={video}
-				onClick={onClick}
-				playOnClick={false}
-				className="post__video"/>
+			<section className="post__video">
+				<Video
+					fit={height ? 'height' : undefined}
+					maxHeight={height}
+					video={video}
+					onClick={onClick}
+					playOnClick={false}/>
+				{video.title &&
+					<h1 className="post__video-title">
+						{url &&
+							<a
+								target="_blank"
+								href={url}>
+								{video.title}
+							</a>
+						}
+						{!url && video.title}
+					</h1>
+				}
+			</section>
 		)
 	}
 }
