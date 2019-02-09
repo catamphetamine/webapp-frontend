8b1d03d69185c16ba53f002525ebeb00dce3ed0c
catamphetamine <purecatamphetamine@gmail.com>
2019-02-09 06:13:02 +0300

    "Not found" page

--- src/routes.common.js
+++ src/routes.common.js
@@ -3,9 +3,11 @@ import Unauthenticated from './pages/Unauthenticated'
 import Unauthorized from './pages/Unauthorized'
 import NotFound from './pages/NotFound'
 
-export const ERROR_PAGES_ROUTES = [
+export const ERROR_PAGES = [
 	{ path: 'unauthenticated', status: 401, Component: Unauthenticated },
 	{ path: 'unauthorized', status: 403, Component: Unauthorized },
 	{ path: 'not-found', status: 404, Component: NotFound },
 	{ path: 'error', status: 500, Component: GenericError }
 ]
+
+export const NOT_FOUND = { path: '*', status: 404, Component: NotFound }
--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,15 @@ import { Redirect } from 'react-website'
 import Application from './pages/Application'
 import AccountProfile from './pages/AccountProfile'
 
-import { ERROR_PAGES_ROUTES } from './routes.common'
+import { ERROR_PAGES, NOT_FOUND } from './routes.common'
 
 export default [{
 	path: '/',
 	Component: Application,
 	children: [
 		new Redirect({ from: '/', to: '/alice' }),
-		...ERROR_PAGES_ROUTES,
-		{ path: ':id', Component: AccountProfile }
+		...ERROR_PAGES,
+		{ path: ':id', Component: AccountProfile },
+		NOT_FOUND
 	]
 }]
