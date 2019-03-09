0f07c71c2be27fba499c14000572d80629a8a238
catamphetamine <purecatamphetamine@gmail.com>
2019-03-09 21:18:33 +0300

    moving stuff around

---
+++ src/utility/post/getHumanReadableLinkAddress.test.js
@@ -0,0 +1,22 @@
+import { describe, it } from '../mocha'
+import expectToEqual from '../expectToEqual'
+import getHumanReadableLinkAddress from './getHumanReadableLinkAddress'
+
+describe('getHumanReadableLinkAddress', () => {
+	it('should get human-readable link address', () => {
+		expectToEqual(
+			getHumanReadableLinkAddress('http://youtube.org'),
+			'youtube.org'
+		)
+
+		expectToEqual(
+			getHumanReadableLinkAddress('https://youtube.org'),
+			'youtube.org'
+		)
+
+		expectToEqual(
+			getHumanReadableLinkAddress('https://www.youtube.org'),
+			'youtube.org'
+		)
+	})
+})
