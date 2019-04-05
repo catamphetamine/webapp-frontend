d69d67bf1f582bef2eac6c628bd99664fa4b9fc1
catamphetamine <purecatamphetamine@gmail.com>
2019-04-05 03:12:21 +0300

    `getInstagramPost()` (won't work in a web browser due to CORS)

--- src/utility/test.js
+++ src/utility/test.js
@@ -1,5 +1,7 @@
 import './getTweet.test'
 import './getTweetText.test'
-import './getInstagramPost.test'
+// Instagram doesn't allow CORS on their oEmbed API.
+// So it doesn't work.
+// import './getInstagramPost.test'
 import './video-youtube.test'
 import './post/test'
