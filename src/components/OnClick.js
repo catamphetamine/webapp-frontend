59e8717353d252761b10f58100d4ef7a30466052
catamphetamine <purecatamphetamine@gmail.com>
2019-06-26 13:51:57 +0300

    Fixed "event is not defined"

--- src/components/OnClick.js
+++ src/components/OnClick.js
@@ -105,7 +105,7 @@ export default class OnClick extends React.Component {
 		)
 	}
 
-	onPointerUp = () => {
+	onPointerUp = (event) => {
 		if (this.isClickInProgress) {
 			this.onPanEnd(
 				event.clientX,
