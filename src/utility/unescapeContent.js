d9ca0c535a4e5176f432e9bb3b3ee90e5d07ff66
catamphetamine <purecatamphetamine@gmail.com>
2019-07-04 19:48:51 +0300

    &apos; -> '

--- src/utility/unescapeContent.js
+++ src/utility/unescapeContent.js
@@ -1,9 +1,11 @@
+// https://en.wikipedia.org/wiki/Character_encodings_in_HTML#HTML_character_references
 export default function unescapeContent(string) {
 	return string
 		.replace(/&quot;/g, '"')
 		.replace(/&amp;/g, '&')
 		.replace(/&gt;/g, '>')
 		.replace(/&lt;/g, '<')
+		.replace(/&apos;/g, '\'')
 		// .replace(/&#39;/g, '\'')
 		// .replace(/&#47;/g, '/')
 		// .replace(/&#92;/g, '\\')
