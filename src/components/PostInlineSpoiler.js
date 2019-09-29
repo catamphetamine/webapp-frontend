28a654521199160560ee53a2eecc37ff3f1f786b
catamphetamine <purecatamphetamine@gmail.com>
2019-09-30 02:59:21 +0300

    Styles refactoring + Moved `quoteAutogenerated` into quotes

--- src/components/PostInlineSpoiler.js
+++ src/components/PostInlineSpoiler.js
@@ -1,50 +1,38 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
 import './PostInlineSpoiler.css'
 
-export default class PostInlineSpoiler extends React.Component {
-	state = {}
-
-	show = () => this.setState({ show: true })
-
+export default function PostInlineSpoiler({
+	hidden,
+	censored,
+	content,
+	children
+}) {
+	const [show, setShow] = useState()
 	// Showing spoiler contents on click is also for
 	// mobile devices which don't have "hover" event.
-	onClick = (event) => {
+	const onClick = useCallback((event) => {
 		// "Prevent default" to disable click fall-through.
 		// (for example, when links are placed inside spoilers).
 		event.preventDefault()
-		this.show()
-	}
-
-	render() {
-		const {
-			hidden,
-			censored,
-			content,
-			children
-		} = this.props
-
-		const {
-			show
-		} = this.state
-
-		return (
-			<span
-				data-hide={!show && hidden ? true : undefined}
-				title={hidden && censored && typeof content === 'string' ? content : undefined}
-				onClick={this.onClick}
-				className={classNames('post__inline-spoiler', {
-					'post__inline-spoiler--hidden': !show && hidden,
-					'post__inline-spoiler--censored': censored
-				})}>
-				<span className="post__inline-spoiler-contents">
-					{children}
-				</span>
+		setShow(true)
+	}, [setShow])
+	return (
+		<span
+			data-hide={!show && hidden ? true : undefined}
+			title={hidden && censored && typeof content === 'string' ? content : undefined}
+			onClick={onClick}
+			className={classNames('post__inline-spoiler', {
+				'post__inline-spoiler--hidden': !show && hidden,
+				'post__inline-spoiler--censored': censored
+			})}>
+			<span className="post__inline-spoiler-contents">
+				{children}
 			</span>
-		)
-	}
+		</span>
+	)
 }
 
 PostInlineSpoiler.propTypes = {
--- src/components/PostQuoteMarker.js
+++ src/components/PostQuoteMarker.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
 import './PostQuoteMarker.css'
 
-export default function PostQuoteMarker() {
+export default function PostQuoteMarker({ className }) {
 	// `<span/>` is used here instead of a `<div/>`
 	// because `<PostQuoteMarker/>` is also used for
 	// `<PostInlineQuote/>`s and `<PostInlineQuoteLink/>`s.
 	return (
-		<span className="post__quote-marker"/>
+		<span className={classNames(className, 'post__quote-marker')}/>
 	)
+}
+
+PostQuoteMarker.propTypes = {
+	className: PropTypes.string
 }
