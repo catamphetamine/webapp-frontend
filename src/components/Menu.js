7631ec03838001e1cf86f5434fef106645979285
catamphetamine <purecatamphetamine@gmail.com>
2019-11-27 05:03:40 +0300

    <Menu/> small refactoring

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -37,60 +37,71 @@ Menu.propTypes = {
 	children: PropTypes.arrayOf(PropTypes.shape(menuItemShape)).isRequired
 }
 
-class MenuItem extends React.Component {
-	render() {
-		let {
-			isSelected
-		} = this.props
-		const {
-			onClick,
-			url,
-			title,
-			icon,
-			iconActive,
-			size,
-			pathname
-		} = this.props
-		const OutlineIcon = icon
-		const FillIcon = iconActive
-		if (url) {
-			isSelected = (isSelected === undefined ? true : isSelected) && pathname === url
-		}
-		// activeClassName={isSelected ? 'menu-item--selected' : undefined}
-		const className = classNames(
-			'menu-item',
-			size && `menu-item--${size}`,
-			isSelected && 'menu-item--selected',
-			iconActive && 'menu-item--fill',
-			!iconActive && 'menu-item--outline'
-		)
-		const children = (
-			<React.Fragment>
-				{FillIcon && <FillIcon className="menu-item__icon menu-item__icon--fill"/>}
-				<OutlineIcon className="menu-item__icon menu-item__icon--outline"/>
-			</React.Fragment>
-		)
-		return (
-			<li>
-				{onClick &&
-					<Button
-						title={title}
-						onClick={onClick}
-						className={classNames('menu-item__button', className)}>
-						{children}
-					</Button>
-				}
-				{url &&
-					<Link
-						to={url}
-						title={title}
-						className={className}>
-						{children}
-					</Link>
-				}
-			</li>
-		)
+function MenuItem({
+	onClick,
+	url,
+	title,
+	icon,
+	iconActive,
+	size,
+	pathname,
+	isSelected,
+	className
+}) {
+	const OutlineIcon = icon
+	const FillIcon = iconActive
+	if (url) {
+		// `url` may contain query parameters.
+		isSelected = (isSelected === undefined ? true : isSelected) && pathname === getPathname(url)
 	}
+	// activeClassName={isSelected ? 'menu-item--selected' : undefined}
+	className = classNames(
+		className,
+		'menu-item',
+		size && `menu-item--${size}`,
+		isSelected && 'menu-item--selected',
+		iconActive && 'menu-item--fill',
+		!iconActive && 'menu-item--outline'
+	)
+	const children = (
+		<React.Fragment>
+			{FillIcon && <FillIcon className="menu-item__icon menu-item__icon--fill"/>}
+			<OutlineIcon className="menu-item__icon menu-item__icon--outline"/>
+		</React.Fragment>
+	)
+	return (
+		<li>
+			{onClick &&
+				<Button
+					title={title}
+					onClick={onClick}
+					className={classNames('menu-item__button', className)}>
+					{children}
+				</Button>
+			}
+			{url &&
+				<Link
+					to={url}
+					title={title}
+					className={className}>
+					{children}
+				</Link>
+			}
+		</li>
+	)
 }
 
 MenuItem.propTypes = menuItemShape
+
+function getPathname(url) {
+	let urlPathname = url
+	const queryIndex = urlPathname.indexOf('?')
+	if (queryIndex >= 0) {
+		urlPathname = urlPathname.slice(0, queryIndex)
+	}
+	const hashIndex = urlPathname.indexOf('#')
+	if (hashIndex >= 0) {
+		urlPathname = urlPathname.slice(0, hashIndex)
+	}
+	return urlPathname
+}
