9ee0d72479eff6770ab55abc63078342443b8ea2
catamphetamine <purecatamphetamine@gmail.com>
2019-11-27 05:02:54 +0300

    <Picture/> comments onerror

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -547,7 +547,13 @@ export function preloadImage(url) {
 	return new Promise((resolve, reject) => {
 		const image = new Image()
 		image.onload = resolve
+		// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#Image_loading_errors
 		image.onerror = (event) => {
+			// // Log the error to `sentry.io`.
+			// setTimeout(() => {
+			// 	console.log(event)
+			// 	throw new Error(event)
+			// }, 0)
 			if (event.path && event.path[0]) {
 				console.error(`Image not found: ${event.path[0].src}`)
 			}
