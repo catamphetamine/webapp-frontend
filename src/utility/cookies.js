83a80f995b47f9ddc06abf7dadb41589425d1cfd
catamphetamine <purecatamphetamine@gmail.com>
2019-12-17 13:41:03 +0300

    `setCookie`

--- src/utility/cookies.js
+++ src/utility/cookies.js
@@ -1,6 +1,8 @@
-// Copy-pasted from:
-// https://learn.javascript.ru/cookie
-
+/**
+ * Reads a cookie.
+ * @param  {string} name
+ * @return {string} [value]
+ */
 export function getCookie(name) {
 	const matches = document.cookie.match(new RegExp(
 		"(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
@@ -8,35 +10,41 @@ export function getCookie(name) {
 	return matches ? decodeURIComponent(matches[1]) : undefined
 }
 
-export function setCookie(name, value, options) {
-	options = options || {}
-
-	let expires = options.expires
-
-	if (typeof expires == "number" && expires) {
-		const date = new Date()
-		date.setTime(date.getTime() + expires * 1000)
-		expires = options.expires = date
+/**
+ * Sets a cookie.
+ * @param {string} name
+ * @param {any} value
+ * @param {object} [options] — All available cookie options (lowercase). https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie
+ * @param {number} [options['max-age']=2147483647] — A cookie is set to "never expire" by default. Setting `max-age` to `-1` preserves the cookie until the browser is closed.
+ */
+export function setCookie(name, value, options = {}) {
+	// `options` will be mutated.
+	options = { ...options }
+	if (options['expires'] instanceof Date) {
+		options['expires'] = options['expires'].toUTCString()
 	}
-	if (expires && expires.toUTCString) {
-		options.expires = expires.toUTCString()
+	// A cookie is set as "never expires" by default
+	if (options['max-age'] === undefined) {
+		// https://stackoverflow.com/a/22479460/970769
+		options['max-age'] = 2147483647
 	}
-
-	value = encodeURIComponent(value)
-
-	let updatedCookie = name + "=" + value
-
+	let cookie = name + "=" + encodeURIComponent(value)
 	for (const propName in options) {
-		updatedCookie += "; " + propName
+		cookie += "; " + propName
 		const propValue = options[propName]
 		if (propValue !== true) {
-			updatedCookie += "=" + propValue
+			cookie += "=" + propValue
 		}
 	}
-
-	document.cookie = updatedCookie
+	document.cookie = cookie
 }
 
+/**
+ * Deletes a cookie.
+ * @param {string} name
+ */
 export function deleteCookie(name) {
-	setCookie(name, '', { 'max-age': -1 })
+	// Setting setMaxAge to `0` will delete the cookie.
+	// Setting it to `-1` will preserve it until the browser is closed.
+	setCookie(name, '', { 'max-age': 0 })
 }
