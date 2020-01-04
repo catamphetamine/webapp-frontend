b33b729783ee35a4718afd478628e59da6a8eec0
catamphetamine <purecatamphetamine@gmail.com>
2020-01-04 12:30:38 +0300

    CountryFlag 3x2 and other flags

--- src/components/CountryFlag.js
+++ src/components/CountryFlag.js
@@ -10,8 +10,8 @@ export default function CountryFlag({ country, name, flagsPath, className }) {
 		<img
 			alt={name}
 			title={name}
-			className={classNames('country-flag', className)}
-			src={`${flagsPath}${getCountryCodeForFlag(country).toLowerCase()}.svg`}/>
+			className={classNames('CountryFlag', className)}
+			src={`${flagsPath}${country}.svg`}/>
 	)
 }
 
@@ -23,32 +23,5 @@ CountryFlag.propTypes = {
 }
 
 CountryFlag.defaultProps = {
-	flagsPath: 'https://lipis.github.io/flag-icon-css/flags/4x3/'
-}
-
-function getCountryCodeForFlag(country) {
-	switch (country) {
-		// "Ascension Island".
-		// The flag is missing for it:
-		// https://lipis.github.io/flag-icon-css/flags/4x3/ac.svg
-		// GitHub issue:
-		// https://github.com/lipis/flag-icon-css/issues/537
-		// Using "SH" flag as a temporary substitute
-		// because previously "AC" and "TA" were parts of "SH".
-		case 'AC':
-			return 'SH'
-
-		// "Tristan da Cunha".
-		// The flag is missing for it:
-		// https://lipis.github.io/flag-icon-css/flags/4x3/ta.svg
-		// GitHub issue:
-		// https://github.com/lipis/flag-icon-css/issues/537
-		// Using "SH" flag as a temporary substitute
-		// because previously "AC" and "TA" were parts of "SH".
-		case 'TA':
-			return 'SH'
-
-		default:
-			return country
-	}
+	flagsPath: 'https://catamphetamine.github.io/country-flag-icons/3x2/'
 }
